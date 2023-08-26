e018a3c9d6017a4373ef29e513dc79d369813273
EdanAlmeida <edluisalmeida@gmail.com>
2023-08-26 17:26:01 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,44 +11,12 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
-//		Connection conn = null;
-//		Statement st = null;
-//		ResultSet rs = null;
-//		
-//		
-//		try {
-//			conn = DB.getConnection();
-//			
-//			st = conn.createStatement();
-//			rs = st.executeQuery("SELECT * FROM DEPARTMENT");
-//			
-//			while (rs.next()) {
-//				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
-//			}
-//			
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		} finally {
-//			DB.closeResultSet(rs);
-//			DB.closeStatement(st);
-//			DB.closeConnection();
-//		}
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Department dpt = new Department();
-		
-		
-		dpt.setId(1);
-		dpt.setName("Books");
-		
-		Seller seller = new Seller(1, "Edan Almeida", "email@email.com", new Date(), 3000.0, dpt);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println(dpt);
+
+		Seller seller = sellerDao.findById(1);
+
 		System.out.println(seller);
-		
 
 	}
 
